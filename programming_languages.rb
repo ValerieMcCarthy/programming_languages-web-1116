03b3edf65ffc211e05c202c4e75110747da8abb2
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-19 08:20:26 -0500

    Languages reformatted.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def reformat_languages(languages) 
+  reformat_hash = {}
+  languages.each do |style, style_hash|
+  		style_hash.each do |lang, lang_hash|
+  		
+  			lang_hash.each do |type, type_result|
+  				if reformat_hash[lang].nil?
+  					reformat_hash[lang] = {}
+  				end
+  				reformat_hash[lang][:style] ||= []
+  				reformat_hash[lang][:style] << style
+  				reformat_hash[lang][:type] = type_result
+  			end
+  		end
+  	end
+  	reformat_hash
 end
